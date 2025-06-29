e3f512d42d89aca6e3b916610e57c1bb65a34535
Cody Farrey <codyfarrey@gmail.com>
2025-06-29 15:49:10 -0500

    updated more stuff but who cares

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,9 @@ export class Footer extends React.Component {
   }
   render () {
     return(
-      <div className="footer header-footer-bar"></div>
+      <div className="footer header-footer-bar">
+        <small>Copyright © 2025 Cody Farrey & Ryan Hugill</small>
+      </div>
     );
   }
 }
